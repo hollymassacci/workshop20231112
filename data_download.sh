51973baf13ceb6e089494866c05a5408f02212a6
hollymassacci <117415409+hollymassacci@users.noreply.github.com>
2023-11-10 16:37:27 -0330

    Create data_download.sh

---
+++ data_download.sh
@@ -0,0 +1,4 @@
+mkdir -p /cloud/project/workshop/data
+cd /cloud/project/workshop
+wget https://github.com/hollymassacci/workshop20231112/blob/main/populations.csv -O data/population.csv
+wget https://github.com/hollymassacci/workshop20231112/blob/main/pelts.csv -O data/pelts.csv
