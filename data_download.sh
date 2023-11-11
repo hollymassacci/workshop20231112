6085cdcedd624d40695052ea7e842f5b0602a46f
hollymassacci <117415409+hollymassacci@users.noreply.github.com>
2023-11-11 16:10:01 -0330

    Add wget download for coding.Rmd

--- data_download.sh
+++ data_download.sh
@@ -2,3 +2,4 @@ mkdir -p /cloud/project/workshop/data
 cd /cloud/project/workshop
 wget https://github.com/hollymassacci/workshop20231112/blob/main/populations.csv -O data/population.csv
 wget https://github.com/hollymassacci/workshop20231112/blob/main/pelts.csv -O data/pelts.csv
+wget https://github.com/hollymassacci/workshop20231112/blob/main/coding.Rmd -O ./coding.Rmd
